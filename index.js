e6704c4a5da3552faa5b563277bef1ff7af4d1bb
Mehedi Hassan <mh93075@gmail.com>
2023-11-07 18:29:55 +0600

    Send token without cors policy and got error

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ async function run() {
 
 
     // Auth realted API
-    
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({token});
+    })
 
 
     // Service related API
